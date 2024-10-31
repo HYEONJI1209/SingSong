0eef76252c899e765e233a6b722f6866deb551d8
HYEONJI1209 <108385746+HYEONJI1209@users.noreply.github.com>
2024-10-31 19:35:27 +0900

    로그인기능

--- frontend/src/components/main/LandingPage.jsx
+++ frontend/src/components/main/LandingPage.jsx
@@ -1,41 +1,63 @@
-import React from "react";
-import { Whatsong} from '../../asset/img/index';
+import React, { useState } from "react";
+import { Whatsong } from '../../asset/img/index';
+import axios from 'axios';
+import { useNavigate, Link } from 'react-router-dom';
+
 const LandingPage = () => {
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('/api/login', {
+                userID: userId,
+                userPW: password,
+            });
+
+            if (response.status === 200) {
+                navigate('/UserPage');
+            } else {
+                alert(response.data.message);
+            }
+
+        } catch (error) {
+            console.error("로그인 요청 중 오류 발생:", error);
+            alert("로그인 중 문제가 발생했습니다.");
+        }
+    };
+
     return (
         <div className="Log">
             <div className="MainLog">
                 <div className="Sing">
-                <img className='Whatsong' alt='Whatsong' src={Whatsong}/>
+                    <img className='Whatsong' alt='Whatsong' src={Whatsong} />
                 </div>
                 <div className="IDPS">
                     <div className="ID">
-                        <div className="inputTitle">
-                            아이디
-                        </div>
+                        <div className="inputTitle">아이디</div>
                         <div className="inputWrap">
-                            <input className="text" type="text" />
+                            <input className="text" type="text" value={userId} onChange={(e) => setUserId(e.target.value)} />
                         </div>
                     </div>
                     <div className="ID">
-                        <div className="inputTitle">
-                            비밀번호
-                        </div>
+                        <div className="inputTitle">비밀번호</div>
                         <div className="inputWrap">
-                        <input className="text" type="text" />
+                            <input className="text" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                         </div>
                     </div>
                     <div className="botlog">
-                        <div className="log">
+                        <div className="log" onClick={handleLogin}>
                             로그인
                         </div>
                         <div className="respass">
-                            <span>회원가입</span>&nbsp;/&nbsp;<span>아이디 찾기</span>&nbsp;/&nbsp;<span>비밀번호 찾기</span>
-                            </div>
+                            <Link to="/signup">회원가입</Link>&nbsp;/&nbsp;<span>아이디 찾기</span>&nbsp;/&nbsp;<span>비밀번호 찾기</span>
+                        </div>
                     </div>
-
                 </div>
             </div>
         </div>
-    )
+    );
 }
+
 export default LandingPage;
